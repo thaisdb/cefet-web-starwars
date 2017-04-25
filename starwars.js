70fe20f59348494c66664ac470554e83bf930b21
thaisdb <thais.dinizbraz@gmail.com>
2017-04-25 11:50:44 -0300

    Add files via upload

--- starwars.js
+++ starwars.js
@@ -3,3 +3,17 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+document.getElementById('movies').innerHTML = '';
+$.ajax({ 
+	url:'http://swapi.co/api/films/',
+	method: 'GET',
+	success: function(resposta) {
+
+		
+		let aux = '';
+		for (let result in resposta){
+			console.log(result);
+			aux = result.title;
+		} 	
+	}
+})
